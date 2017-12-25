f5cbafca93a45ec1143bd0a6bf3065fc4a141b74
HoneyPatouceul <30809346+HoneyPatouceul@users.noreply.github.com>
2017-12-25 23:43:35 +0100

    Create setup-aarch64.py

---
+++ setup-aarch64.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import os
+os.environ['CFLAGS'] = "-I/usr/include/libusb-1.0"
+os.environ['LDFLAGS'] = "-L/usr/lib/aarch64-linux-gnu -lusb-1.0 -ludev -lrt"
+
+setup(
+cmdclass = {'build_ext': build_ext},
+ext_modules = [Extension("hid", ["hid.pyx", "hid-libusb.c"],
+libraries=["usb-1.0", "udev", "rt"])]
+)
